027cb9a78309602bfc55141b13d76db08b9f8ea6
Madhukarkumar12 <madhukarkumar26072001@gmail.com>
2026-09-07 17:33:02 +0530

    feat: implemented product api except search functionality

---
+++ src/main/java/com/ecom/ecom_application/service/ProductService.java
@@ -0,0 +1,81 @@
+package com.ecom.ecom_application.service;
+
+import com.ecom.ecom_application.dto.ProductRequest;
+import com.ecom.ecom_application.dto.ProductResponse;
+import com.ecom.ecom_application.model.Product;
+import com.ecom.ecom_application.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest){
+        Product product = new Product();
+        updateProductFromRequest(product, productRequest);
+        Product savedProduct = productRepository.save(product);
+        return mapToProductResponse(savedProduct);
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        return productRepository.findByActiveTrue().stream()
+                .map(this::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<ProductResponse> updateProduct(Long id, ProductRequest productRequest){
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    updateProductFromRequest(existingProduct, productRequest);
+                    Product savedProduct = productRepository.save(existingProduct);
+                    return mapToProductResponse(savedProduct);
+                });
+    }
+
+    public boolean deleteProduct(Long id){
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setActive(false);
+                    productRepository.save(existingProduct);
+                    return true;
+                }).orElse(false);
+
+    }
+
+//    public List<ProductResponse> searchProducts(String keyword){
+//
+//
+//    }
+
+
+
+    public void updateProductFromRequest(Product product, ProductRequest productRequest){
+        product.setName(productRequest.getName());
+        product.setDescription(productRequest.getDescription());
+        product.setPrice(productRequest.getPrice());
+        product.setStockQuantity(productRequest.getStockQuantity());
+        product.setCategory(productRequest.getCategory());
+        product.setImageUrl(productRequest.getImageUrl());
+    }
+
+    public ProductResponse mapToProductResponse(Product product){
+        ProductResponse response = new ProductResponse();
+        response.setId(product.getId());
+        response.setName(product.getName());
+        response.setDescription(product.getDescription());
+        response.setPrice(product.getPrice());
+        response.setCategory(product.getCategory());
+        response.setImageUrl(product.getImageUrl());
+        response.setActive(product.getActive());
+        response.setStockQuantity(product.getStockQuantity());
+
+        return response;
+    }
+
+}
